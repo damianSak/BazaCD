35ed82ce34aaed0bbe238c2719448e142b4e644c
damianSak <damian.sak88@gmail.com>
2020-03-20 00:24:00 +0100

    .Initial changes from 22.03.2020 00:22 Fix adding duplicated record

--- src/main/java/com/melon/bazacd/actions/AddRecord.java
+++ src/main/java/com/melon/bazacd/actions/AddRecord.java
@@ -11,7 +11,7 @@ import java.util.*;
 public class AddRecord {
 
     private Scanner scanner = new Scanner(System.in);
-    private String s = " ";
+
 
     protected void addRecord(List<Album> albums) {
         String band;
@@ -19,42 +19,54 @@ public class AddRecord {
         String genre;
         int releaseDate;
         int actualYear = Calendar.getInstance().get(Calendar.YEAR);
+        String s;
+        List<Album> repet = new LinkedList<>();
 
         do {
             do {
                 System.out.println("Podaj nazwę zespołu");
                 band = scanner.nextLine();
-                if(band.isEmpty()) {
+                if (band.isEmpty()) {
                     System.out.println("Nie podano rzadnej nazwy");
                 }
             } while (band.isEmpty());
             do {
                 System.out.println("Podaj tytuł płyty: ");
                 title = scanner.nextLine();
-                if(title.isEmpty()) {
+                if (title.isEmpty()) {
                     System.out.println("Nie podano rzadnej nazwy płyty");
                 }
             } while (title.isEmpty());
+
+            for (Album a : albums) {
+                if (a.getBand().equals(band) && a.getTitle().equals(title)) {
+                    repet.add(a);
+                }
+            }
+            if (repet.size() > 0) {
+                System.out.println("Wprowadzony album z podanym wykonawcą już istnieje na tej liście, spróbuj ponownie \n");
+                addRecord(albums);
+            }
             do {
                 System.out.println("Podaj gatunek wykonywanej muzyki: ");
                 genre = scanner.nextLine();
-                if(genre.isEmpty()) {
+                if (genre.isEmpty()) {
                     System.out.println("Nie podano rzadnego gatunku");
                 }
-            }while (genre.isEmpty());
+            } while (genre.isEmpty());
             do {
                 System.out.println("Podaj rok wydania albumu: ");
                 releaseDate = scanner.nextInt();
-                if(releaseDate<1887||releaseDate>actualYear)
-                System.out.println("Nie podano roku wydania płyty lub data wykracza poza możliwy relany historyczny zakres ");
-            }while (releaseDate<1887||releaseDate>actualYear );
-            albums.add(new Album(band, title, genre, releaseDate));
+                if (releaseDate < 1887 || releaseDate > actualYear)
+                    System.out.println("Nie podano roku wydania płyty lub data wykracza poza możliwy relany historyczny zakres ");
+            } while (releaseDate < 1887 || releaseDate > actualYear);
+
+                albums.add(new Album(band, title, genre, releaseDate));
 
             System.out.println("Wprowadź 'T'/'t' aby dodać kolejną pozycję lub 'N'/'n' aby wrócić do MENU: ");
-
             s = scanner.nextLine();
-
-        } while (s.equals("T") || s.equals("t"));
+        }
+        while (s.equals("T") || s.equals("t"));
 
     }
 }
